6f9c313c01a796e5f392f05311ad71b2272eff6f
Angelino Pires <lino.pires@live.com>
2019-09-11 13:58:57 -0300

    Ajustes de tamanho nas badges e rooms

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { faCopy, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
@@ -15,12 +16,33 @@ export class CarouselComponent implements OnInit {
 	faCopy = faCopy;
 	faSignInAlt = faSignInAlt;
 
-	constructor() { }
+	constructor(private router: Router) { }
 
 	ngOnInit() {
 	}
 
 	getPercentage(current: number, total: number): number {
 		return Number(((current / total) * 100).toFixed());
+	}
+
+	navigateTo(route: string, url?: string): void {
+		if (url) {
+			console.log(route, url);
+
+			this.router.navigate([route, { externalUrl: url }]);
+			return;
+		}
+		this.router.navigate([route]);
+	}
+
+	copyText(text: string) {
+		let selBox = document.createElement('textarea');
+		selBox.classList.add('invisible');
+    selBox.value = text;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
 	}
 }
--- src/app/components/fun-room/fun-room.component.ts
+++ src/app/components/fun-room/fun-room.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
+
 import { FunModel } from 'src/app/core/models/FunModel';
 
 @Component({
-  selector: 'app-fun-room',
-  templateUrl: './fun-room.component.html',
-  styleUrls: ['./fun-room.component.scss']
+	selector: 'app-fun-room',
+	templateUrl: './fun-room.component.html',
+	styleUrls: ['./fun-room.component.scss']
 })
 export class FunRoomComponent implements OnInit {
 	@Input() funLobby: FunModel;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
+	ngOnInit() {
 	}
 }
